4b7cbad9d4a2f5e65f965c0b6cffe07d0cb2584e
jaeheonYoo325 <yoojh0325@gmail.com>
2022-07-31 11:21:49 +0900

    2022.07.31 : 트랜잭션 - 적용2.

---
+++ src/test/java/com/springdb1/springdb1jdbc/repository/MemberRepositoryV2Test.java
@@ -0,0 +1,65 @@
+package com.springdb1.springdb1jdbc.repository;
+
+import com.springdb1.springdb1jdbc.domain.Member;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static com.springdb1.springdb1jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV2Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() {
+        //기본 DriverManager - 항상 새로운 커넥션을 획득
+        //DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV100", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findByid(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member != findMember {}", member == findMember);
+        log.info("member equals findMember {}", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
+
+        //update: money : 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findByid(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findByid(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/test/java/com/springdb1/springdb1jdbc/service/MemberServiceV2Test.java
+++ src/test/java/com/springdb1/springdb1jdbc/service/MemberServiceV2Test.java
@@ -1,9 +1,9 @@
 package com.springdb1.springdb1jdbc.service;
 
-import com.springdb1.springdb1jdbc.connection.ConnectionConst;
 import com.springdb1.springdb1jdbc.domain.Member;
 import com.springdb1.springdb1jdbc.repository.MemberRepositoryV1;
-import org.assertj.core.api.Assertions;
+import com.springdb1.springdb1jdbc.repository.MemberRepositoryV2;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,26 +13,27 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import java.sql.SQLException;
 
 import static com.springdb1.springdb1jdbc.connection.ConnectionConst.*;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
- * 기본 동작, 트랜잭션이 없어서 문제 발생
+ * 트랜잭션 - 커넥션 파라미터 전달 방식 동기화
  */
-class MemberServiceV1Test {
+@Slf4j
+class MemberServiceV2Test {
 
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
 
-    private MemberRepositoryV1 memberRepository;
-    private MemberServiceV1 memberService;
+    private MemberRepositoryV2 memberRepository;
+    private MemberServiceV2 memberService;
 
     @BeforeEach
     void before() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        memberRepository = new MemberRepositoryV1(dataSource);
-        memberService = new MemberServiceV1(memberRepository);
+        memberRepository = new MemberRepositoryV2(dataSource);
+        memberService = new MemberServiceV2(dataSource, memberRepository);
     }
 
     @AfterEach
@@ -52,7 +53,9 @@ class MemberServiceV1Test {
         memberRepository.save(memberB);
 
         //when
+        log.info("START TX");
         memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        log.info("END TX");
 
         //then
         Member findMemberA = memberRepository.findByid(memberA.getMemberId());
@@ -77,7 +80,7 @@ class MemberServiceV1Test {
         //then
         Member findMemberA = memberRepository.findByid(memberA.getMemberId());
         Member findMemberB = memberRepository.findByid(memberEx.getMemberId());
-        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberA.getMoney()).isEqualTo(10000);
         assertThat(findMemberB.getMoney()).isEqualTo(10000);
     }
 }
