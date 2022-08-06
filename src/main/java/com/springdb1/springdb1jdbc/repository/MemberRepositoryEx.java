8281afc8a2f0024446988ea4a8a492bfbda9fec5
jaeheonYoo325 <yoojh0325@gmail.com>
2022-08-06 17:37:56 +0900

    2022.08.06 : 체크 예외와 인터페이스.

---
+++ src/main/java/com/springdb1/springdb1jdbc/repository/MemberRepositoryEx.java
@@ -0,0 +1,12 @@
+package com.springdb1.springdb1jdbc.repository;
+
+import com.springdb1.springdb1jdbc.domain.Member;
+
+import java.sql.SQLException;
+
+public interface MemberRepositoryEx {
+    Member save(Member member) throws SQLException;
+    Member findByid(String memberId) throws SQLException;
+    void update(String memberId, int money) throws SQLException;
+    void delete(String memberId) throws SQLException;
+}
